9a94ede70e92a32f2a124072dd60938c9b7ac855
muajadev <ericks28@gmail.com>
2020-10-30 18:40:34 -0600

    actualizado

---
+++ src/app/component/tiendas/tiendasdetalle.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "tiendadetalle-app",
+  templateUrl: "./tiendasdetalle.component.html",
+  styleUrls: ["./tiendasdetalle.component.css"]
+})
+export class TiendasDetalleComponent {
+  tiendaDetalle = "Actualizando";
+}
